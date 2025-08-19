6d92720c89b0e5ec06018a4a93e1984e5f8ea5e4
Kavyashree SN <snkavyashree25@gmail.com>
2025-08-19 17:27:17 +0530

    Added solution for check if array is sorted and rotated

---
+++ sortedarr.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool check(vector<int>& arr) {
+        int n = arr.size();
+        int count = 0;
+
+       
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < arr[i - 1]) {
+                count++;
+            }
+        }
+
+        
+        if (arr[n - 1] > arr[0]) {
+            count++;
+        }
+
+        return count <= 1;
+    }
+};
+
+int main() {
+    Solution s;
+
+    vector<int> arr1 = {3,4,5,1,2};
+    vector<int> arr2 = {2,1,3,4};
+    vector<int> arr3 = {1,2,3};
+
+    cout << (s.check(arr1) ? "true" : "false") << endl; 
+    cout << (s.check(arr2) ? "true" : "false") << endl; 
+    cout << (s.check(arr3) ? "true" : "false") << endl; 
+
+    return 0;
+}
