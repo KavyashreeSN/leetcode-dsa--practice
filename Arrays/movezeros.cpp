40d5896ae92b326a148a481f01d92a40669a6c05
Kavyashree SN <snkavyashree25@gmail.com>
2025-08-24 12:37:38 +0530

    Added moveZeroes.cpp solution

---
+++ Arrays/movezeros.cpp
@@ -0,0 +1,34 @@
+//283. Move Zeros
+//Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int j = 0; 
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {0, 1, 0, 3, 12};
+
+    sol.moveZeroes(nums);
+
+    cout << "Result: ";
+    for (int x : nums) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
