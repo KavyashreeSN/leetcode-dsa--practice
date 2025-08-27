f47fc12d8d5972cd918805c2b34f98907a1f63e1
KavyashreeSN <snkavyashree25@gmail.com>
2025-08-27 12:53:07 +0530

    Update linearsearch.cpp

--- Arrays/linearsearch.cpp
+++ Arrays/linearsearch.cpp
@@ -1,3 +1,6 @@
+/*Given an array of integers nums and an integer target, find the smallest index (0 based indexing) where the target appears in the array.
+If the target is not found in the array, return -1*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
