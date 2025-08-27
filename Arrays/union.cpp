5a329906f625de4c46208d6beed1ee5a35c4b461
KavyashreeSN <snkavyashree25@gmail.com>
2025-08-27 13:01:26 +0530

    Update union.cpp

--- Arrays/union.cpp
+++ Arrays/union.cpp
@@ -1,3 +1,7 @@
+/*Given two sorted arrays nums1 and nums2, return an array that contains the union of these two arrays. The elements in the union must be in ascending order.
+
+The union of two arrays is an array where all values are distinct and are present in either the first array, the second array, or both*/
+
 #include<bits/stdc++.h>
 using namespace std;
  class solution{
