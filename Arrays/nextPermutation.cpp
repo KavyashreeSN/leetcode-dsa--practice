a1e370fa1fdce14d174afe8813c9a77218cbf793
Kavyashree SN <snkavyashree25@gmail.com>
2025-08-29 21:28:37 +0530

    Added nextPermutation.cpp solution

---
+++ Arrays/nextPermutation.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int ind = -1;
+
+        // Step 1: Find the first index "ind" from the right
+        // where nums[ind] < nums[ind+1]
+        for(int i = n - 2; i >= 0; i--) {
+            if(nums[i] < nums[i + 1]) {
+                ind = i;
+                break;
+            }
+        }
+
+        // Step 2: If no such index exists, it's the last permutation
+        if(ind == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        // Step 3: Find element just larger than nums[ind] from the right side
+        for(int i = n - 1; i > ind; i--) {
+            if(nums[i] > nums[ind]) {
+                swap(nums[i], nums[ind]);
+                break;
+            }
+        }
+
+        // Step 4: Reverse the subarray from ind+1 to end
+        reverse(nums.begin() + ind + 1, nums.end());
+    }
+};
