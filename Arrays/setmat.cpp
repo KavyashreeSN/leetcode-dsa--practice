fabb24623151e2f50ec6bd2de3c078ae3de7d0b3
Kavyashree SN <snkavyashree25@gmail.com>
2025-09-02 21:05:51 +0530

    Added solution for Set Matrix Zeroes

---
+++ Arrays/setmat.cpp
@@ -0,0 +1,75 @@
+/*73. SET MATRIX ZEROES
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+TC -->O(N*M)
+SC -->O(1)
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int col0 = 1;
+
+       
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    if(j != 0) {
+                        matrix[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+
+        
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j < m; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        
+        if(matrix[0][0] == 0) {
+            for(int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+       
+        if(col0 == 0) {
+            for(int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
+
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 1, 1},
+        {1, 0, 1},
+        {1, 1, 1}
+    };
+
+    Solution().setZeroes(matrix);
+
+    cout << "Resultant Matrix:\n";
+    for(auto &row : matrix) {
+        for(auto &val : row) {
+            cout << val << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
