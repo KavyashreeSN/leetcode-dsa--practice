c523d2fdcada9c5630f42f333da20cc39b77e093
Kavyashree SN <snkavyashree25@gmail.com>
2025-09-03 15:22:27 +0530

    Added solution for Binary Search

---
+++ Binary Search/binary_search.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(const vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2; 
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1; 
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {-1, 0, 3, 5, 9, 12};
+    int target = 9;
+    int result = sol.search(nums, target);
+    if (result != -1)
+        cout << "Target " << target << " found at index: " << result << endl;
+    else
+        cout << "Target " << target << " not found in the array." << endl;
+
+    return 0;
+}
