48b67fbc4b3bce993ca842f35293ae72122f59be
Kavyashree SN <snkavyashree25@gmail.com>
2025-09-04 21:12:23 +0530

    Add .gitignore and stop tracking build/editor files

--- Arrays/subarr_sum.cpp
+++ Arrays/subarr_sum.cpp
@@ -1,3 +1,12 @@
+/*560. Subarray Sum Equals k
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+TC --> O(N*logN)
+SC -->O(N)*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
