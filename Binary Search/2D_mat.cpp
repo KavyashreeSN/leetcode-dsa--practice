5d0794d8f636783e4bf4ddbd13461459b1c68c58
Kavyashree SN <snkavyashree25@gmail.com>
2025-09-21 12:54:53 +0530

    Added solution for 2D Matrix||

---
+++ Binary Search/2D_mat.cpp
@@ -0,0 +1,54 @@
+/*240. Search in 2D matrix.
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+
+TC -->O(n+m)
+SC -->O(1)*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int row = 0, col = m - 1;
+
+        while (row < n && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                row++; // move down
+            } else {
+                col--; // move left
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<vector<int>> matrix = {
+        {1, 4, 7, 11, 15},
+        {2, 5, 8, 12, 19},
+        {3, 6, 9, 16, 22},
+        {10, 13, 14, 17, 24},
+        {18, 21, 23, 26, 30}
+    };
+
+    int target = 16;
+
+    if (sol.searchMatrix(matrix, target)) {
+        cout << "Target " << target << " found in the matrix." << endl;
+    } else {
+        cout << "Target " << target << " not found in the matrix." << endl;
+    }
+
+    return 0;
+}
