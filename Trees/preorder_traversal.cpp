7d485840b8c8f88d70c0f179cb0448f90595258a
Kavyashree SN <snkavyashree25@gmail.com>
2025-09-28 19:17:28 +0530

    Added solution for preorder traversal

---
+++ Trees/preorder_traversal.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        preorder(root, result);
+        return result;
+    }
+
+private:
+    void preorder(TreeNode* node, vector<int>& res) {
+        if (!node) return;
+        res.push_back(node->val);       // Visit root
+        preorder(node->left, res);      // Visit left
+        preorder(node->right, res);     // Visit right
+    }
+};
+
+int main() {
+    // Create a sample binary tree:
+    //        1
+    //         \
+    //          2
+    //         /
+    //        3
+    TreeNode* root = new TreeNode(1);
+    root->right = new TreeNode(2);
+    root->right->left = new TreeNode(3);
+
+    Solution sol;
+    vector<int> result = sol.preorderTraversal(root);
+
+    cout << "Preorder Traversal: ";
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0; 
+}
+
