4d43e7df72749ab175d8cee3d06fcf71abadf448
Kavyashree SN <snkavyashree25@gmail.com>
2025-10-05 21:12:24 +0530

    Added solution for postorder traversal

---
+++ Trees/iterative_postorder.cpp
@@ -0,0 +1,71 @@
+/* Iterative Postorder Traversal
+
+Given root of binary tree, return the postorder traversal of the binary tree.
+
+TC -->O(n)
+SC -->O(n)(worst case)*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) {
+        val = x;
+        left = right = NULL;
+    }
+};
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> postorder;
+        if (root == NULL) return postorder;
+
+        stack<TreeNode*> st1, st2;
+        st1.push(root);
+        while (!st1.empty()) {
+            TreeNode* node = st1.top();
+            st1.pop();
+            st2.push(node);
+
+            if (node->left != NULL)
+                st1.push(node->left);
+            if (node->right != NULL)
+                st1.push(node->right);
+        }
+        while (!st2.empty()) {
+            postorder.push_back(st2.top()->val);
+            st2.pop();
+        }
+        return postorder;
+    }
+};
+
+int main() {
+    // Create a sample tree:
+    //         1
+    //        / \
+    //       2   3
+    //      / \
+    //     4   5
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    Solution sol;
+    vector<int> result = sol.postorderTraversal(root);
+
+    cout << "Postorder Traversal: ";
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
