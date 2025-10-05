b31bbff5f272ab288f17b34d6250a9809b8fae44
Kavyashree SN <snkavyashree25@gmail.com>
2025-10-05 20:55:52 +0530

    Added solution for iterative preorder traversal

---
+++ Trees/iterative_preorder.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) {
+        val = x;
+        left = right = NULL;
+    }
+};
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preorder;
+        if (root == NULL) return preorder;
+
+        stack<TreeNode*> st;
+        st.push(root);
+        while (!st.empty()) {
+            root = st.top();
+            st.pop();
+            preorder.push_back(root->val);
+
+            // Push right first so left is processed first
+            if (root->right != NULL)
+                st.push(root->right);
+            if (root->left != NULL)
+                st.push(root->left);
+        }
+        return preorder;
+    }
+};
+
+int main() {
+    // Create a sample tree:
+    //         1
+    //        / \
+    //       2   3
+    //      / \
+    //     4   5
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    Solution sol;
+    vector<int> result = sol.preorderTraversal(root);
+
+    cout << "Preorder Traversal: ";
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
