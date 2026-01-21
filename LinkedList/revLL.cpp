488b4d31eae217751f710c92feda6d7260bf97c1
John <john@gmail.com>
2026-01-21 11:40:58 +0530

    recursive approach

---
+++ LinkedList/revLL.cpp
@@ -0,0 +1,52 @@
+//Recursive approach
+
+#include <iostream>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        ListNode* newHead = reverseList(head->next);
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = NULL;
+
+        return newHead;
+    }
+};
+
+int main() {
+    // Create linked list: 1 -> 2 -> 3 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    Solution sol;
+    head = sol.reverseList(head);
+
+    // Print reversed list
+    ListNode* temp = head;
+    while (temp != NULL) {
+        cout << temp->val << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL";
+
+    return 0;
+}
