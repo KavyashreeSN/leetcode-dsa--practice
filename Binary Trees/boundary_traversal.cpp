62b981eb8f6e81d11808c96e91ebd2d91487bd90
KavyashreeSN <snkavyashree25@gmail.com>
2026-07-14 19:33:42 +0530

    Added solution for boundary traversal

---
+++ Binary Trees/boundary_traversal.cpp
@@ -0,0 +1,98 @@
+/*Boundary Traversal Of Binary Tree
+
+Given a root of Binary Tree, perform the boundary traversal of the tree. 
+
+The boundary traversal is the process of visiting the boundary nodes of the binary tree in the anticlockwise direction, starting from the root.
+The boundary of a binary tree is the concatenation of the root, the left boundary, the leaves ordered from left-to-right, and the reverse order of the right boundary.
+
+The left boundary is the set of nodes defined by the following:
+
+The root node's left child is in the left boundary. 
+If the root does not have a left child, then the left boundary is empty.
+If a node in the left boundary and has a left child, then the left child is in the left boundary.
+If a node is in the left boundary, has no left child, but has a right child, then the right child is in the left boundary.
+
+The leftmost leaf is not in the left boundary.
+The right boundary is similar to the left boundary, except it is the right side of the root's right subtree. Again, the leaf is not part of the right boundary, and the right boundary is empty if the root does not have a right child.*/
+
+
+//TC-->O(N)
+//SC-->O(N)
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+class Solution {
+    bool isLeaf(Node* node) {
+        return (node->left == nullptr && node->right == nullptr);
+    }
+
+    void addLeftBoundary(Node* root, vector<int>& res) {
+        Node* cur = root->left;
+        while (cur) {
+            if (!isLeaf(cur)) res.push_back(cur->data);
+            if (cur->left) cur = cur->left;
+            else cur = cur->right;
+        }
+    }
+
+    void addRightBoundary(Node* root, vector<int>& res) {
+        Node* cur = root->right;
+        vector<int> tmp;
+        while (cur) {
+            if (!isLeaf(cur)) tmp.push_back(cur->data);
+            if (cur->right) cur = cur->right;
+            else cur = cur->left;
+        }
+        for (int i = tmp.size() - 1; i >= 0; --i) {
+            res.push_back(tmp[i]);
+        }
+    }
+
+    void addLeaves(Node* root, vector<int>& res) {
+        if (isLeaf(root)) {
+            res.push_back(root->data);
+            return;
+        }
+        if (root->left) addLeaves(root->left, res);
+        if (root->right) addLeaves(root->right, res);
+    }
+
+public:
+    vector<int> boundary(Node* root) {
+        vector<int> res;
+        if (!root) return res;
+        if (!isLeaf(root)) res.push_back(root->data);
+        addLeftBoundary(root, res);
+        addLeaves(root, res);
+        addRightBoundary(root, res);
+        return res;
+    }
+};
+
+int main() {
+    // Example
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    Solution sol;
+    vector<int> ans = sol.boundary(root);
+    for (int x : ans) cout << x << " ";
+}
